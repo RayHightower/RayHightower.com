873031a745c344c014b0fb58d313a2de1f281165
Raymond T. Hightower <rth@wisdomgroup.com>
2015-12-29 20:19:13 -0600

    add content to the list of fields that gets searched

--- js/search.js
+++ js/search.js
@@ -3,7 +3,8 @@ jQuery(function() {
   // a boost of 10 to indicate matches on this field are more important.
   window.idx = lunr(function () {
     this.field('id');
-    this.field('title', { boost: 10 });
+    this.field('title');
+    this.field('content', { boost: 10 });
     this.field('author');
     this.field('categories');
   });
