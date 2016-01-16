795ef7e9e6fa3a71f759f225fa15900f0515a17e
Raymond T. Hightower <rth@wisdomgroup.com>
2016-01-16 11:05:06 -0600

    point to the eventDefault line. possible culprit in Firefox issue

--- js/search.js
+++ js/search.js
@@ -22,7 +22,7 @@ jQuery(function() {
 
   // Event when the form is submitted
   $("#site_search").submit(function(){
-      event.preventDefault();
+      event.preventDefault(); // RTH: per Google, preventDefault() might be teh culprit in Firefox
       var query = $("#search_box").val(); // Get the value for the text field
       var results = window.idx.search(query); // Get lunr to perform a search
       display_search_results(results); // Hand the results off to be displayed
